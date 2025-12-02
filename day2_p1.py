396bb3dd2b7ef052626e82945f0a4c2dcb0f9300
Marieke <mariekevdp@gmail.com>
2025-12-02 13:23:00 +0000

    day 2 complete

---
+++ day2_p1.py
@@ -0,0 +1,21 @@
+f = open('day2_input.txt')
+product_IDs = f.read().split(',')
+
+invalid_IDs = []
+
+for product_ID in product_IDs:
+
+    x = product_ID.find('-')
+    firstID = int(''.join(product_ID[:x]))
+    lastID = int(''.join(product_ID[x+1:]))
+
+    for ID in range(firstID,lastID+1,1):
+        IDstr = str(ID)
+        string_length = len(IDstr)
+
+        if string_length % 2 == 0: #must be even
+            if (IDstr[:int(string_length/2)]) == (IDstr[int(string_length/2):]):
+                invalid_IDs.append(ID)
+                print('invalid ID found', ID)
+
+print(sum(invalid_IDs))
---
+++ day2_p2.py
@@ -0,0 +1,36 @@
+f = open('day2_input.txt')
+product_IDs = f.read().split(',')
+
+invalid_IDs = []
+
+for product_ID in product_IDs:
+
+    x = product_ID.find('-')
+    firstID = int(''.join(product_ID[:x]))
+    lastID = int(''.join(product_ID[x+1:]))
+
+    for ID in range(firstID,lastID+1,1):
+        invalid_flag=0
+        IDstr = str(ID)
+        string_length = len(IDstr)
+
+        for i in range(string_length+1):
+            divisor=i+1
+
+            if string_length % divisor == 0 and divisor <= string_length/2: #the string length must be divisible and will only ever be a max length of 1/2
+                substring_array = []
+                no_of_substrings = int(string_length/divisor)
+
+                for index_of_substring in range(no_of_substrings):
+                    start_index = (index_of_substring*divisor)
+                    end_index = (index_of_substring*divisor)+divisor
+                    substring_array.append(IDstr[start_index:end_index])
+                    
+                if len(set(substring_array)) == 1:
+                    invalid_flag = 1
+                    
+        if invalid_flag == 1:
+            invalid_IDs.append(ID)
+
+
+print(sum(invalid_IDs))
