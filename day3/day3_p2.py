5f86abd8e59e06d74eb6c93925f8bd6a084cc32f
Marieke <mariekevdp@gmail.com>
2025-12-03 23:47:32 +0000

    day3 p2 tidy

--- day3/day3_p2.py
+++ day3/day3_p2.py
@@ -9,19 +9,14 @@ for batterybank in batterybank_list:
 
     first_allowed_index = 0
     
-    print(batterybank)
     for i in range(12):
         final_allowed_index = len(batterybank)-(11-i)
-        print(i, first_allowed_index, final_allowed_index)
 
         current_max = max(batterybank[first_allowed_index:final_allowed_index])
 
         joltarray.append(current_max)
 
         first_allowed_index += batterybank[first_allowed_index:final_allowed_index].index(current_max)+1   
-
-    print(joltarray)
-    print(int(''.join(joltarray)))
 
     max_joltage_list.append(int(''.join(joltarray)))
 
